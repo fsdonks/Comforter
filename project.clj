d3c5b1d46b0ccab74fda8fae1410e4ae9a79e3b8
Craig <whoknows@net.com>
2018-08-08 12:17:58 -0400

    correct typos and rearrange project.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject comforter.clj "0.0.1-SNAPSHOT"
+(defproject Comforter.clj "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
-  :aot [compforter.core]
-  :main compforter.core)
+  :aot [Comforter.core]
+  :main Comforter.core)
