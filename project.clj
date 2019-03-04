6edce7250ed5cf411b676073836a68dd4c19d854
tom <tom@that-place>
2019-03-04 15:35:56 -0600

    bah

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject comforter "0.0.1-SNAPSHOT"
   :description "Post processor for requirements analysis."
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]]
   :aot [comforter.core]
   :main comforter.core)
